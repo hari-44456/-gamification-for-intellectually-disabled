6fa938273d65a8c9ee147220a10303a3323ffa8c
kakarot_6909 <rahul.naravadkar@gmail.com>
2020-10-17 19:21:52 +0530

    Refactor. Uses the react bootstrap in Cards.jsx

--- frontend/src/Card/Cards.jsx
+++ frontend/src/Card/Cards.jsx
@@ -1,4 +1,7 @@
 import React,{Component}from 'react';
+import {Container} from '@material-ui/core';
+import {Row,Col} from 'react-bootstrap';
+
 import Card from './CardsUI';
 
 import img1 from '../assert/1.jpg';
@@ -7,28 +10,25 @@ import img3 from '../assert/3.jpg';
 import img4 from '../assert/4.jpg';
 class Cards extends Component{
     render(){
-
         return(
             <div>
-                 <h1>Student DashBoard</h1>
-            <div className='container-fluid d-flex justify-content-center'>
-               
-               <div className='row'>
-                   <div className='col-md-5'>
-                       <Card imgsrc={img1} title="Fluid Reasoning" path="/G1"/>
-                   </div>
-                   <div className='col-md-5'>
-                       <Card imgsrc={img2} title="Quantatitive Reasoning" path="/G2"/>
-                   </div>
-                   <div className='col-md-5'>
-                       <Card imgsrc={img3} title="Visual-Spatial Processing" path="/G3"/>
-                       
-                   </div>
-                   <div className='col-md-5'>
-                       <Card imgsrc={img4} title="Working Memory" path="/G4"/>
-                   </div>
-                </div>
-            </div> 
+                <h1>Student DashBoard</h1>
+                <Container>
+                    <Row>
+                        <Col md={6} align='center'>
+                            <Card imgsrc={img1} title="Fluid Reasoning" path="/G1"/>
+                        </Col>
+                        <Col md={6} align='center'>
+                            <Card imgsrc={img2} title="Quantatitive Reasoning" path="/G2"/>
+                        </Col>
+                        <Col md={6} align='center'>
+                            <Card imgsrc={img3} title="Visual-Spatial Processing" path="/G3"/> 
+                        </Col>
+                        <Col md={6} align='center'>
+                            <Card imgsrc={img4} title="Working Memory" path="/G4"/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
