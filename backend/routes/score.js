25a4e5f25141b59b9e519e01bb621637e188f3a2
wceacses <clubacses@gmail.com>
2020-11-12 10:48:45 +0530

    added /scores route...use this route for updating any game's score

--- backend/routes/score.js
+++ backend/routes/score.js
@@ -2,14 +2,9 @@ const router = require('express').Router();
 const StudentScore = require('../models/StudentScore');
 const { verifyToken } = require('../verify');
 
-router.put('/g1', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
 	try {
 		const { sid = -1, g1 = -1, g2 = -1, g3 = -1, g4 = -1 } = req.body;
-
-		if (sid == -1 || g1 == -1)
-			return res
-				.status(400)
-				.send('Please Provide student id and score of g1 game');
 
 		StudentScore.findOne({ sid }, async (err, doc) => {
 			if (err) return res.status(400).send('Error...');
@@ -37,39 +32,26 @@ router.put('/g1', async (req, res) => {
 			}
 
 			// Update doc
-			const getIndex = (scores) => {
-				let i = 0;
-				scores.forEach((item) => {
-					if (
-						item.date.search(
-							new Date().toISOString().substring(0, 10),
-						) != -1
-					)
-						return i;
-					i++;
-				});
+			const getIndex = (scores, match) => {
+				for (let i = 0; i < scores.length; i++)
+					if (scores[i].date.localeCompare(match) == 0) return i;
 				return -1;
 			};
 
-			const index = getIndex(doc.scores);
-			console.log(index);
+			const index = getIndex(
+				doc.scores,
+				new Date().toISOString().substring(0, 10),
+			);
 
 			if (index == -1) doc.scores.push({ g1, g2, g3, g4 });
 			else {
-				console.log(doc.scores[index]);
 				if (g1 > doc.scores[index].g1) doc.scores[index].g1 = g1;
 				if (g2 > doc.scores[index].g2) doc.scores[index].g2 = g2;
 				if (g3 > doc.scores[index].g3) doc.scores[index].g3 = g3;
 				if (g4 > doc.scores[index].g4) doc.scores[index].g4 = g4;
 			}
 			try {
-				console.log(JSON.stringify(doc.scores));
-				const result2 = await doc.updateOne(
-					{ sid },
-					{
-						$set: { scores: doc.scores },
-					},
-				);
+				const result2 = await doc.save();
 				return res
 					.status(200)
 					.send(`updated successfully...${JSON.stringify(result2)}`);
